a1ea917e5cf2b38612e131bb792ad14df4d62354
saud maruli <uthrul@gmail.com>
2018-12-20 08:56:27 +0700

    add twitter wall on about page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
 
     def index
-        @posts= Post.all.order("created_at desc").paginate(page: params[:page], per_page: 2)
+        @posts= Post.all.order("created_at desc").paginate(page: params[:page], per_page: 3)
     end
 
     def new
